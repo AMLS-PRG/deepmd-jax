b78f5d4e819be351a608ae9de44144e040043979
Pablo Peña <pablopcbo@gmail.com>
2025-11-17 00:45:21 +0100

    Setup update

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools.command.install import install
 
 setup(
     name='deepmd_jax',
-    version='0.2',
     packages=find_packages(),
     install_requires=[
         'jax[cuda12]>=0.7.1',
